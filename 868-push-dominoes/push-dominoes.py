cc8eec5728f60cc58f72766afce277092fd2ff02
Abiy Shiferaw <abiyshiferaw12@gmail.com>
2025-05-03 00:47:53 +0300

    Time: 259 ms (39.94%) | Memory: 22.7 MB (50.83%) - LeetSync

---
+++ 868-push-dominoes/push-dominoes.py
@@ -0,0 +1,34 @@
+class Solution:
+    def pushDominoes(self, dominoes: str) -> str:
+        n = len(dominoes)
+        forces = [0] * n
+        # Right Forces
+        force = 0
+        for i in range(n):
+            if dominoes[i] == "R":
+                force = n
+            elif dominoes[i] == "L":
+                force = 0
+            else:
+                force = max(force - 1, 0)
+            forces[i] += force
+        # Left Forces
+        force = 0
+        for i in range(n-1, -1, -1):
+            if dominoes[i] == "L":
+                force = n
+            elif dominoes[i] == "R":
+                force = 0
+            else:
+                force = max(force - 1, 0)
+            forces[i] -= force
+        result = []
+        for f in forces:
+            if f > 0:
+                result.append("R")
+            elif f < 0:
+                result.append("L")
+            else:
+                result.append(".")
+        return "".join(result)
+        
