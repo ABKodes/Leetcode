80a2f926ddbf9e27719dbd25bf342d996eb32dcb
Abiy Shiferaw <abiyshiferaw12@gmail.com>
2025-07-18 22:42:28 +0300

    Time: 0 ms (100.00%) | Memory: 18 MB (19.46%) - LeetSync

--- 1303-minimum-moves-to-reach-target-score/minimum-moves-to-reach-target-score.py
+++ 1303-minimum-moves-to-reach-target-score/minimum-moves-to-reach-target-score.py
@@ -1,29 +1,5 @@
 class Solution:
-    def minMoves(self, target: int, maxDoubles: int) -> int:
-        # Can you run it and check 
-        
-        #  I'm just testing sth
-        # Did you understand the question
-        # def power(n):
-        #     return n > 0 and math.log2(n).is_integer()
-        # moves = 0
-        # while target > 1 and maxDoubles > 0:
-        #     if maxDoubles > 0 and power(target):
-        #         target = target // 2
-        #         moves += 1
-        #         maxDoubles -= 1
-        #     elif maxDoubles > 0 and not power(target):
-        #         target = target - 1
-        #         moves += 1
-        #     elif maxDoubles == 0:
-        #         moves += target - 1
-        #         break
-        # print(moves)
-        # why are we getting time limit exceeded 
-        # I don't know i'm just checking it 
-        # I didn't name the function right that's why let me check i
-        
-        
+    def minMoves(self, target: int, maxDoubles: int) -> int:        
         moves = 0
         while target > 1 and maxDoubles > 0:
             if target % 2 == 0:
